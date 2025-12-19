52b1f05f93fd66184b3e78cdccb74489548b1c61
Siva Krishna Kumar Lokam <89242145+krishnalokam@users.noreply.github.com>
2025-12-19 15:53:33 +0530

    Add initial auth backend setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT||5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('Auth Backend is running ');
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on ${PORT}`)
+});
