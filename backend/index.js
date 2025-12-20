93cfa37b86ef5bfbad306f513c90de42090ea9bf
Krishna Lokam <siva.lokam4work@gmail.com>
2025-12-20 17:08:51 +0530

    Connect backend to MongoDB Atlas using Mongoose

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const connectDB = require("./config/db");
+
 const PORT = process.env.PORT||5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Connect DB
+connectDB();
 
 app.get('/',(req,res) => {
     res.send('Auth Backend is running ');
